28cd44a97609669b1b2d49ef759bfdbc002fdabd
RameshwariS <shrutisatpute1112@gmail.com>
2026-01-08 15:28:45 +0530

    basic

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+// const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+const mongoose  = require('mongoose')
+
+//load env
+dotenv.config();
+
+const PORT = process.env.PORT || 8000
+const app = express()
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/home',(req,res)=>{
+    res.send("HI")
+})
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("DB connected!");
+
+    app.listen(PORT, () => {
+      console.log(`Server started on ${PORT}!!`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });
